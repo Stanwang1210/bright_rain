c61d4253bc3dcf2c61718d4ebbd1a1bd53020bc5
Stanwang1210 <ch995308@gmail.com>
2020-08-10 16:34:13 +0800

    'upload'

---
+++ script/test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# run testing process
+
+# $1 : Experiment name
+# $2 : Cuda id
+
+CONFIG="dlhlp_test"
+DIR="/work/b07u1234/b06502162/Lu_model1"
+
+echo "Start running testing process of E2E ASR"
+CUDA_VISIBLE_DEVICES=$2 python3 main.py --config config/${CONFIG}.yaml \
+    --name $1 \
+	--test \
+    --njobs 8 \
+    --seed 0 \
+    --ckpdir ${DIR}/ckpt/$1 \
+	--outdir ${DIR}/test_result/$1
+
+# Eval
+python3 eval.py --file ${DIR}/test_result/$1/$1_dev_output.csv
+python3 eval.py --file ${DIR}/test_result/$1/$1_test_output.csv
---
+++ script/train_lm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+CONFIG="librispeech_lm"
+DIR="/work/b07u1234/b06502162/Lu_model1"
+
+echo "Start running training process of RNNLM"
+CUDA_VISIBLE_DEVICES=$2 python3 main.py --config config/${CONFIG}.yaml \
+    --name $1 \
+    --njobs 8 \
+    --seed 0 \
+    --lm \
+    --logdir ${DIR}/log/ \
+    --ckpdir ${DIR}/ckpt/ \
+    --outdir ${DIR}/result/ \
